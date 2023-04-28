18776ec52165189828c7863c368d9db6c29dfef3
un4rch <81522320+un4rch@users.noreply.github.com>
2023-04-28 10:48:46 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,1 +1,1 @@
-pip install emoji pandas scikit-learn imbalanced-learn nltk emot mixed_naive_bayes gensim
+pip install emoji pandas scikit-learn imbalanced-learn nltk emot mixed_naive_bayes gensim chardet
